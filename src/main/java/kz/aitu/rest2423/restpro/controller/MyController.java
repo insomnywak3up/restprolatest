832b711caaeeb3a0b2b368aac5442a0d4c52f3d4
ВашеИмя <Email@example.com>
2025-01-23 10:21:55 +0500

    Initial commit

---
+++ src/main/java/kz/aitu/rest2423/restpro/controller/MyController.java
@@ -0,0 +1,14 @@
+package kz.aitu.rest2423.restpro.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/main")
+    public String Mylistener()
+    {
+        return "Hello World";
+    }
+}
